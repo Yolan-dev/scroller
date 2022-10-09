42d7c91fea0cb62243b42f4322da328462dc4e20
Yolan <ivan.shrub@bk.ru>
2022-10-09 22:36:54 +0600

    add scroll anim & pic

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
+import Swiper from 'swiper';
+import 'swiper/swiper.scss';
 
-
-  
+const swiper = new Swiper('.showcaseCarousel', {
+  loop: true,
+  slidesPerView: 3,
+  speed: 1800,
+  centeredSlides: true,
 });
+document.querySelector('video').playbackRate = 2;
