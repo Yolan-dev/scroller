8af07b0bc1346b0d8891dd87fdefe0e5205c6db7
Yolan <ivan.shrub@bk.ru>
2022-10-13 16:42:08 +0600

    add navigation & improved anim

--- src/js/common.js
+++ src/js/common.js
@@ -2,9 +2,13 @@ import Swiper from 'swiper';
 import 'swiper/swiper.scss';
 
 const swiper = new Swiper('.showcaseCarousel', {
-  loop: true,
+  // loop: true,
   slidesPerView: 3,
   speed: 1800,
   centeredSlides: true,
+  navigation: {
+    nextEl: '.showcase-navigation__next',
+    prevEl: '.showcase-navigation__prev',
+  },
 });
 document.querySelector('video').playbackRate = 2;
